b6956cd1d69a349a3d1868db984c519a93a49ebe
liukai <alikai812@qq.com>
2020-10-12 23:25:29 +0800

    add MAGIC

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 //#include "person.pb.h"
-#include "msg.pb.h"
-#include <fstream>
+#include "mg.pb.h"
+#include <unistd.h>
+
+ #include <fcntl.h>
 
 
+//#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/io/coded_stream.h>
+#include <libprotobuf/android/armeabi-v7a/include/google/protobuf/io/zero_copy_stream_impl.h>
 
 
-
-
+using namespace google::protobuf::io;
 void gb()
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     char buf[1024] = {'\0'};
     int buf_len = 0;
-    msg_t msg1;
-    msg1.set_id(1001);
+    MAGIC_CODE msg;
+    msg.set_key(7890);
+    msg.set_macaddress(123456);
+    msg.set_serilnumber(7890);
+    msg.set_mgaction(MAGIC_CODE_MagicCodeAction_VERIFYMAGICCODE);
+//    msg.SerializeWithCachedSizes();
+    int fd = open("myfile", O_WRONLY|O_CREAT,0600);
+    if(fd<0){
+        printf("fd error\n");
+        return;
+    }
+    google::protobuf::io::ZeroCopyOutputStream* raw_output = new OstreamOutputStream(fd);
 
-    // *) serialize phrase => object to byte array
-    msg1.SerializeToArray(buf, sizeof(buf));
-    buf_len = msg1.ByteSize();
-    msg_t msg2;
-    // *) deserialize phrase => byte array to object
-    msg2.ParseFromArray(buf, buf_len);
-    assert(msg1.id() == msg2.id());
+    auto* coded_output = new CodedOutputStream(raw_output);
+    msg.SerializeWithCachedSizes(coded_output);
+
+
+    MAGIC_CODE rev;
+
+
+
+
+
+
 }
 int main(){
-gb();
+    gb();
     return 0;
 }
